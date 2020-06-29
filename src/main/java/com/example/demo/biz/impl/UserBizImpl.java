5913cf6bf86ae0dce872eba86b7b665ea74d675d
tianbj <19981125>
2020-06-29 17:36:07 +0800

    整合redis缓存

--- src/main/java/com/example/demo/biz/impl/UserBizImpl.java
+++ src/main/java/com/example/demo/biz/impl/UserBizImpl.java
@@ -1,8 +1,12 @@
 package com.example.demo.biz.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.biz.UserBiz;
 import com.example.demo.dao.UserDao;
+import com.example.demo.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +14,13 @@ import java.util.List;
 
 @Service
 public class UserBizImpl implements UserBiz {
+
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private RedisUtils redisUtils;
+
     @Override
     public int deleteByPrimaryKey(Integer id) {
         return userDao.deleteByPrimaryKey(id);
@@ -29,16 +38,28 @@ public class UserBizImpl implements UserBiz {
 
     @Override
     public UserEntity selectByPrimaryKey(Integer id) {
-        return userDao.selectByPrimaryKey(id);
+        //取redis数据
+        if(redisUtils.hasKey(id+"")){
+            UserEntity user = JSONObject.toJavaObject((JSON) redisUtils.get(id+""),UserEntity.class);
+            return (user) ;
+        } else {
+            UserEntity user = userDao.selectByPrimaryKey(id);
+            redisUtils.set(id+"",user);
+            return user;
+        }
     }
 
     @Override
     public int updateByPrimaryKeySelective(UserEntity record) {
+        //修改后删除缓存
+        redisUtils.SyncValue(record.getId()+"");
         return userDao.updateByPrimaryKeySelective(record);
     }
 
     @Override
     public int updateByPrimaryKey(UserEntity record) {
+        //修改后删除缓存
+        redisUtils.SyncValue(record.getId()+"");
         return userDao.updateByPrimaryKey(record);
     }
 
